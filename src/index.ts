57198070478c6ada9399308069e42ab924887bba
ing.dacastillop <ing.dacastillop@gmail.com>
2025-01-04 11:47:51 -0500

    :arrow_up: Upgrade typescript version 5.0 in project

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ type EachError = <T>(element: T, index: number) => void;
 
 interface ArrayEachOptions<T> {
   array: T[];
-  each: EachCallback;
+  fnEach: EachCallback;
   catchError?: EachError;
 }
 
 interface MapOptions<E, V> {
-  factory: (element: E) => V;
+  elementToValue: (element: E) => V;
   identifier: (element: E) => string;
-  reducer: (element: E, currentValue: V) => void;
+  reducer: (element: E, value: V) => void;
 }
 
 type Reducer<T, V> = (value: T) => V;
@@ -28,19 +28,19 @@ export function inArray<T>(array: T[], element: T): boolean {
   return array.indexOf(element) !== -1;
 }
 
-export function firstElement<T>(array: T[]): Nulleable<T> {
+export function first<T>(array: T[]): Nulleable<T> {
   return array.length === 0 ? null : array[0];
 }
 
-export function lastElement<T>(array: T[]): Nulleable<T> {
+export function last<T>(array: T[]): Nulleable<T> {
   return array.length === 0 ? null : array[array.length - 1];
 }
 
-export function pushElement<T>(array: T[], element: T): T[] {
+export function push<T>(array: T[], element: T): T[] {
   return [...array, element];
 }
 
-export function replaceElement<T>(
+export function refresh<T>(
   array: T[],
   element: T,
   criteria: (element: T) => boolean
@@ -50,33 +50,28 @@ export function replaceElement<T>(
   );
 }
 
-export function removeElement<T>(
-  array: T[],
-  criteria: (element: T) => boolean
-): T[] {
+export function destroy<T>(array: T[], criteria: (element: T) => boolean): T[] {
   return array.filter((currentElement) => !criteria(currentElement));
 }
 
-export function removeIndex<T>(array: T[], index: number): T[] {
+export function remove<T>(array: T[], index: number): T[] {
   return array.filter((_, currentIndex) => index !== currentIndex);
 }
 
-export function arrayEach<T>(options: ArrayEachOptions<T>): boolean {
-  const { array, each: each, catchError: stop } = options;
+export function each<T>(options: ArrayEachOptions<T>): boolean {
+  const { array, fnEach, catchError } = options;
 
   try {
     array.forEach((element, index) => {
-      if (each(element, index)) {
+      if (fnEach(element, index)) {
         throw new ForEachBreakException(element, index);
       }
     });
 
     return true;
   } catch (error) {
-    if (stop && error instanceof ForEachBreakException) {
-      const { element, index } = error;
-
-      stop(element, index);
+    if (catchError && error instanceof ForEachBreakException) {
+      catchError(error.element, error.index);
     }
 
     return false;
@@ -87,37 +82,30 @@ export function reduceDistinct<T, V>(array: T[], reducer: Reducer<T, V>): V[] {
   return array.reduce((result: V[], element) => {
     const value = reducer(element);
 
-    if (!result.includes(value)) {
-      result.push(value);
-    }
+    !result.includes(value) && result.push(value);
 
     return result;
   }, []);
 }
 
 export function mapToReduce<E, V>(array: E[], options: MapOptions<E, V>): V[] {
-  const { factory, identifier, reducer } = options;
-
   const collection = new Map<string, V>();
 
   function currentValue(element: E): V {
-    const resultId = identifier(element);
+    const resultId = options.identifier(element);
 
-    const value = collection.get(resultId);
+    let value = collection.get(resultId);
 
-    if (value) {
-      return value;
+    if (!value) {
+      value = options.elementToValue(element);
+      collection.set(resultId, value);
     }
 
-    const newValue = factory(element);
-
-    collection.set(resultId, newValue);
-
-    return newValue;
+    return value;
   }
 
   array.forEach((element) => {
-    reducer(element, currentValue(element));
+    options.reducer(element, currentValue(element));
   });
 
   return Array.from(collection.entries()).map(([_, value]) => value);
